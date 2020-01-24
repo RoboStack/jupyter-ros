a292e995f6b6baaae020b214551cb2b0a1bfc858
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-01-24 11:46:45 +0100

    fix rtd build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,8 @@ extensions = [
     # 'jupyter_sphinx.embed_widgets',
 ]
 
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -56,7 +58,7 @@ exclude_patterns = []
 #
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-htmlhelp_basename = 'ipycanvasdoc'
+htmlhelp_basename = 'jupyterrosdoc'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
