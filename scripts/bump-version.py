93c60a923b2645205ed0b89efcfa94ab64ab6ac1
Carlos Herrero <contact@carloshb.com>
2022-08-08 17:46:40 +0200

    Fixes build workflow, updates manifest and bump version

--- scripts/bump-version.py
+++ scripts/bump-version.py
@@ -12,30 +12,40 @@ from pathlib import Path
 from jupyter_releaser.util import get_version, run
 from pkg_resources import parse_version
 
-LERNA_CMD = "jlpm run lerna version --no-push --force-publish --no-git-tag-version"
-
 def _update_version(version):
-    HERE = Path(__file__).parent.resolve()
+    HERE = Path(__file__).parent.parent.resolve()
 
     for settings in HERE.rglob("package.json"):
         try:
-            with settings.open() as f:
-                version = json.load(f)["version"]
-                print(version)
+            with settings.open('r') as f:
+                pckg_json = json.load(f)
+            
+            pckg_json['version'] = version
+
+            with settings.open('w') as f:
+                json.dump(pckg_json, f, indent=2)
+            
+            # Check version
+            with settings.open('r') as f:
+                ver = json.load(f)['version']
+                print("package.json version:", ver)
+            
+            return
+                
         except FileNotFoundError:
             pass
 
     raise FileNotFoundError(f"Could not find package.json under dir {HERE!s}")
 
 @click.command()
-@click.option("--force", default=False, is_flag=True)
 @click.argument("spec", nargs=1)
-def bump(force, spec):
+def bump(spec):
     status = run("git status --porcelain").strip()
     if len(status) > 0:
         raise Exception("Must be in a clean git state with no untracked files")
 
     curr = parse_version(get_version())
+
     if spec == 'next':
         spec = f"{curr.major}.{curr.minor}."
         if curr.pre:
@@ -62,13 +72,7 @@ def bump(force, spec):
         js_version += f"-{p}.{x}"
 
     # bump the JS packages
-    lerna_cmd = f"{LERNA_CMD} {js_version}"
-    if force:
-        lerna_cmd += " --yes"
-    #run(lerna_cmd)
-
     _update_version(js_version)
-    print(js_version)
 
 
 if __name__ == "__main__":
