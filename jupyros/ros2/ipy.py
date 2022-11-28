9c00b2eae453b968bd00b30afb1f6ec9aab1bae2
ldania <luigi.r.dania@gmail.com>
2022-11-28 10:34:54 +0000

    Adjustestment of thread_cell

--- jupyros/ros2/ipy.py
+++ jupyros/ros2/ipy.py
@@ -5,6 +5,8 @@
 #                                                                           #
 # The full license is in the file LICENSE, distributed with this software.  #
 #############################################################################
+
+## Modified by Luigi Dania for Jupyter-Ros2
 import sys
 from threading import Thread
 
@@ -17,7 +19,7 @@ def executor(cell, gbls, lcls):
 # @register_cell_magic is not available during jupyter nbextension enable ...
 try:
     @register_cell_magic
-    def thread_cell(line, cell, local_ns=None):
+    def thread_cell2(line, cell, local_ns=None):
         t = Thread(target=executor, args=(cell, globals(), sys._getframe(2).f_locals))
         out = widgets.Output(layout={'border': '1px solid gray'})
         t.start()
